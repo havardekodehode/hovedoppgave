637a9b36d253eff632b07138cf4ff1923a199d60
havardekodehode <havarde.kodehode@gmail.com>
2023-08-21 15:03:24 +0200

    trying to update readme SS

---
+++ js/data/api.js
@@ -0,0 +1,11 @@
+// /**
+//  *
+//  * @param {*} url
+//  * @returns
+//  */
+// export async function getData(url) {
+//     const req = await fetch(url);
+//     const data = await req.json();
+
+//     return data;
+// }
