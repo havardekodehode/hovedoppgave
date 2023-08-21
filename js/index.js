e62e4990bca09ac9cbd52f1463d06a6109c95caf
havardekodehode <havarde.kodehode@gmail.com>
2023-08-21 14:41:11 +0200

    BG color now updates based on pokemon type

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // import {getData} from "./data/api.js"
-
-const mainContainer = document.querySelector(".dexContainer");
+import { renderPokedex } from "./pages/pokedex.js";
+import { dexContainer } from "./HTMLElements.js";
+import { getData } from "./data/data.js";
 
 const baseUrl = "https://pokeapi.co/api/v2/pokemon?limit=151";
 // const baseUrl = "https://pokeapi.co/api/v2/pokemon/1/";
@@ -22,15 +23,9 @@ document
         () => (index !== 151 ? index++ : null, navigate(baseUrl))
     );
 
-async function getData(url) {
-    const req = await fetch(url);
-    const data = await req.json();
-    return data;
-}
-
 async function navigate(url) {
     const data = await getData(url);
-    mainContainer.innerHTML = "";
+    dexContainer.innerHTML = "";
     if (navState == "main") {
     } else {
         const pokemonList = data.results;
@@ -44,113 +39,4 @@ async function navigate(url) {
     }
 }
 
-function renderPokedex(pokemonData, details) {
-    const pokedexContainer = createElement("div", { className: "pokedex" });
-    const pokemonContainer = createElement("div", {
-        className: "pokemonContainer",
-    });
-    const pokemonName = createElement("h3", {
-        style: "text-align: center",
-        textContent:
-            pokemonData.name.charAt(0).toUpperCase() +
-            pokemonData.name.slice(1),
-        class: `${pokemonData.name}`,
-    });
-    const pokemonImage = createElement("img", {
-        classList: `pokemon ${pokemonData.name}`,
-        src: details.sprites.other["official-artwork"].front_default,
-        alt: `Sprite of ${pokemonData.name}`,
-    });
-    const container = createElement("div", { className: "stats" });
-
-    const pokemonStats = details.stats.map(({ base_stat, stat }) => {
-        const statAndValcontainer = createElement("div", {
-            className: "statAndValue",
-        });
-        let statName = "";
-        if (!stat.name.includes("special")) {
-            statName = createElement("p", {
-                textContent: stat.name.toUpperCase(),
-                className: "stat",
-            });
-        } else {
-            const specialStatName =
-                stat.name.substring(0, 2) +
-                ". " +
-                stat.name.substring(8, 11) +
-                ".";
-            statName = createElement("p", {
-                textContent: specialStatName.toUpperCase(),
-                className: "stat",
-            });
-        }
-
-        const statValue = createElement("span", {
-            textContent: base_stat,
-            className: "value",
-        });
-        statAndValcontainer.append(statName, statValue);
-
-        return statAndValcontainer;
-    });
-
-    container.append(...pokemonStats);
-    pokemonContainer.append(pokemonName, pokemonImage, container);
-    pokedexContainer.append(pokemonContainer);
-    mainContainer.append(pokedexContainer);
-}
-
 navigate(baseUrl);
-
-const createElement = (type = "div", props = {}) => {
-    const element = document.createElement(type);
-    Object.entries(props).forEach(([key, value]) => (element[key] = value));
-    return element;
-};
-
-// Load SVGS
-
-// function addSVG(container, url) {
-//     getSVGDoc(url).then((doc) => {
-//         let svgEl = doc.querySelector("svg");
-
-//         container.append(svgEl);
-//     });
-// }
-
-// /**
-//  * @param {string} url - URL for the svg file
-//  * @returns {SVGSVGElement} - Returns the SVG element, of the given file in the url
-//  */
-// function getSVGDoc(url) {
-//     return new Promise((resolve, reject) => {
-//         fetch(url)
-//             .then((response) => response.text())
-//             .then((str) =>
-//                 resolve(
-//                     new window.DOMParser().parseFromString(str, "image/svg+xml")
-//                 )
-//             )
-//             .catch((error) => {
-//                 reject(error);
-//             });
-//     });
-// }
-
-// function render() {
-//     window.onload = () => {
-//         getSVGDoc("images/rucksack.svg").then((doc) => {
-//             // console.log(doc);
-//             const svgEl = doc.getElementById("openingOpened");
-//             console.log(svgEl);
-//             svgEl.classList.toggle("slideIn");
-//             setTimeout(() => {
-//                 addSVG(mainContainer, "images/rucksack.svg");
-//                 // svgEl.classList.toggle("slideIn");
-//                 console.log("saødkfjhaslkd");
-//             }, 3000);
-//         });
-//     };
-// }
-
-// render();
---
+++ js/pages/pokedex.js
@@ -0,0 +1,83 @@
+import { dexContainer } from "../HTMLElements.js";
+import { createElement } from "../utils/DOMManipulation.js";
+
+const typeColors = [
+    { type: "normal", color: "A8A77A" },
+    { type: "fire", color: "EE8130" },
+    { type: "water", color: "6390F0" },
+    { type: "electric", color: "F7D02C" },
+    { type: "grass", color: "7AC74C" },
+    { type: "ice", color: "96D9D6" },
+    { type: "fighting", color: "C22E28" },
+    { type: "poison", color: "A33EA1" },
+    { type: "ground", color: "E2BF65" },
+    { type: "flying", color: "A98FF3" },
+    { type: "psychic", color: "F95587" },
+    { type: "bug", color: "A6B91A" },
+    { type: "rock", color: "B6A136" },
+    { type: "ghost", color: "735797" },
+    { type: "dragon", color: "6F35FC" },
+    { type: "dark", color: "705746" },
+    { type: "steel", color: "B7B7CE" },
+    { type: "fairy", color: "D685AD" },
+];
+
+export function renderPokedex(pokemonData, details) {
+    const pokedexContainer = createElement("div", { className: "pokedex" });
+    const pokemonContainer = createElement("div", {
+        className: "pokemonContainer",
+    });
+    const pokemonName = createElement("h3", {
+        style: "text-align: center",
+        textContent:
+            pokemonData.name.charAt(0).toUpperCase() +
+            pokemonData.name.slice(1),
+        class: `${pokemonData.name}`,
+    });
+    const pokemonImage = createElement("img", {
+        classList: `pokemon ${pokemonData.name}`,
+        src: details.sprites.other["official-artwork"].front_default,
+        alt: `Sprite of ${pokemonData.name}`,
+    });
+    const container = createElement("div", { className: "stats" });
+
+    const pokemonStats = details.stats.map(({ base_stat, stat }) => {
+        const statAndValcontainer = createElement("div", {
+            className: "statAndValue",
+        });
+        let statName = "";
+        if (!stat.name.includes("special")) {
+            statName = createElement("p", {
+                textContent: stat.name.toUpperCase(),
+                className: "stat",
+            });
+        } else {
+            const specialStatName =
+                stat.name.substring(0, 2) +
+                ". " +
+                stat.name.substring(8, 11) +
+                ".";
+            statName = createElement("p", {
+                textContent: specialStatName.toUpperCase(),
+                className: "stat",
+            });
+        }
+
+        const statValue = createElement("span", {
+            textContent: base_stat,
+            className: "value",
+        });
+        statAndValcontainer.append(statName, statValue);
+
+        return statAndValcontainer;
+    });
+
+    pokedexContainer.style.backgroundColor = `#${
+        typeColors.find((entry) => entry.type == details.types[0].type.name)
+            .color
+    }`;
+    container.append(...pokemonStats);
+    pokemonContainer.append(pokemonName, pokemonImage, container);
+    pokedexContainer.append(pokemonContainer);
+    dexContainer.append(pokedexContainer);
+}
